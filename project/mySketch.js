9e1edb1c327737d400d353367909787de20902b1
Quentinwllms <123713551+Quentinwllms@users.noreply.github.com>
2025-01-09 17:52:20 +0000

    submitting my code

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,8 +1,22 @@
+let colors = ["blue", "red", "green", "yellow"];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
+  background(100);
 }
 
 function draw() {
-  background(200);
-  
+  if (mouseIsPressed === true) {
+  eye(mouseX, mouseY);
+  }
+}
+
+function eye(x,y) {
+
+  noStroke();
+  fill(random(colors));
+  rect(x,y,100,80);
+
+  fill(random(colors));
+  ellipse(x+50,y+40,50,40);
 }
