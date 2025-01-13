533fe3e81f37eeb4fe124e13cbb1b18b632a172a
Quentinwllms <123713551+Quentinwllms@users.noreply.github.com>
2025-01-13 23:23:03 +0000

    Quentin's eyes

--- project/mySketch.js
+++ project/mySketch.js
@@ -43,36 +43,31 @@ function setup() {
   openEye();
 }
 
-function draw() {
-}
-
 function eye() {
  
   strokeWeight(1.5)
 
   randomColor = floor(random(0,colors.length));
   fill(colors[randomColor]);
-  arc (x,y,w,h,180,0);
-  arc (x,y,w,h,0,180);
+  ellipse(x,y,w,h); // left eye
   
-  arc(x+200,y,w,h,0,180);
-  arc(x+200,y,w,h,180,0);
+  ellipse(x + 200,y,w,h); //right eye
 
   fill(0);
 
-  ellipse(x + pupilChangeX,y - pupilChangeY + 5,30,30);
-  ellipse(x + pupilChangeX + 200,y - pupilChangeY + 5,30,30);
+  ellipse(x + pupilChangeX,y - pupilChangeY + 5,30,30); //left pupil
+  ellipse(x + pupilChangeX + 200,y - pupilChangeY + 5,30,30); //right pupil
   fill(255);
-  ellipse(x + pupilChangeX + 5,y - pupilChangeY - 5,4,4);
-  ellipse(x + pupilChangeX + 205,y - pupilChangeY - 5,4,4);
+  ellipse(x + pupilChangeX + 5,y - pupilChangeY - 5,5,5); //left highlight
+  ellipse(x + pupilChangeX + 205,y - pupilChangeY - 5,5,5); //right highlight
 }
 
 
 function blink () {
   background (200);
   strokeWeight(8);
-  line(x-w/2,y,x+w/2,y);
-  line(x + 200 - w/2,y,x + 200 + w/2,y);
+  line(x-w/2,y,x+w/2,y); //left closed eye
+  line(x + 200 - w/2,y,x + 200 + w/2,y); //right closed eye
   
   setTimeout(openEye,205);
 }
@@ -84,16 +79,16 @@ function openEye () {
   if (openTime[randomOpenTime] == "wink") {
   wink();
     
-  setTimeout(blink,500);
+  setTimeout(openEye,500);
   }
   else {
   background(200);
-  pupilChangeX = random(-20,20);
-  pupilChangeY = random(1.2,8.2);
+  pupilChangeX = random(-20,20); //randomizes pupil position in eye
+  pupilChangeY = random(1.3,8.1);
   eye();
   
   //console.log(randomOpenTime);
-  setTimeout(blink,openTime[randomOpenTime]);
+  setTimeout(blink,openTime[randomOpenTime]); //sets timeout randomized from set values in array
     }
 }
 
@@ -101,15 +96,14 @@ function wink () {
   background(200);
   strokeWeight(1.5);
 
-  randomColor = floor(random(0,colors.length));
+ // randomColor = floor(random(0,colors.length));
   fill(colors[randomColor]);
-  arc (x,y,w,h,180,0);
-  arc (x,y,w,h,0,180);
+  ellipse(x,y,w,h);
   
   fill(0);
   ellipse(x ,y  ,30,30);
   fill(255);
-  ellipse(x+5,y-10,4,4);
+  ellipse(x+5,y-10,5,5);
   
   strokeWeight(8);
   line(x+157,y,x+243,y-20);
